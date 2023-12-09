aa49293946633953fe2e825b6300b0be8e5202ec
Dan Diaconeasa <mkr@Dans-MacBook-Pro.local>
2023-12-09 14:03:22 +0200

    second commit

--- userextend/urls.py
+++ userextend/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from userextend import views
 
 urlpatterns = [
-    path('create_user', views.UserCreateView.as_view, name='create-user')
+    path('create_user/', views.UserCreateView.as_view(), name='create-user')
 ]
