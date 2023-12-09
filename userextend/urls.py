58614212f4bc04e1672bdeddfd5628445b685ff9
Dan Diaconeasa <mkr@Dans-MacBook-Pro.local>
2023-12-09 14:35:49 +0200

    second commit

--- userextend/urls.py
+++ userextend/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from userextend import views
 
 urlpatterns = [
-    path('create_user/', views.UserCreateView.as_view(), name='create-user')
+    path('create_user/', views.UserCreateView.as_view(), name='create-user'),
+    path('logout/', views.logout_view, name='logout')
 ]
--- userextend/views.py
+++ userextend/views.py
@@ -1,8 +1,9 @@
+from django.contrib.auth import logout
 from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-
+from django.shortcuts import redirect
 from userextend.forms import UserForm
 
 
@@ -11,3 +12,7 @@ class UserCreateView(CreateView):
     model = User
     form_class = UserForm
     success_url = reverse_lazy('login')
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
