22220279bc74523bf1843827bfbe57f55bb9c715
Dan Diaconeasa <mkr@Dans-MacBook-Pro.local>
2023-12-10 12:28:25 +0200

    second commit

--- createPost/views.py
+++ createPost/views.py
@@ -12,10 +12,17 @@ class CreatePostView(CreateView):
     form_class = AnuntForm
     success_url = reverse_lazy('home')
 
+def comision_0_view(request):
+    anunturi = Anunt.objects.filter(tip_anunt='C0')
+    return render(request, 'createPost/comision_0.html', {'anunturi': anunturi})
 
-class ListeView:
-    pass
+def ansamblu_rezidential_view(request):
+    anunturi = Anunt.objects.filter(tip_anunt='AR')
+    return render(request, 'createPost/ansamblu_rezidential.html', {'anunturi': anunturi})
 
+def agentii_imobiliare_view(request):
+    anunturi = Anunt.objects.filter(tip_anunt='AI')
+    return render(request, 'createPost/agentii_imobiliare.html', {'anunturi': anunturi})
 
 class Comision0View(ListView):
     template_name = 'createPost/comision_0.html'
@@ -31,15 +38,3 @@ class AnsambluRezidentialView(ListView):
     template_name = 'createPost/ansamblu_rezidential.html'
     model = Anunt
     context_object_name = 'all_announce'
-
-def comision_o_view(request):
-    anunturi = Anunt.objects.filter(tip_anunt='C0')
-    return render(request, 'createPost/comision_0.html', {'anunturi': anunturi})
-
-def ansamblu_rezidential_view(request):
-    anunturi = Anunt.objects.filter(tip_anunt='AR')
-    return render(request, 'createPost/ansamblu_rezidential.html', {'anunturi': anunturi})
-
-def agentii_imobiliare_view(request):
-    anunturi = Anunt.objects.filter(tip_anunt='AI')
-    return render(request, 'createPost/agentii_imobiliare.html', {'anunturi': anunturi})
