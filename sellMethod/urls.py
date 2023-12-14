d53e229477080121049acf82dc55d31e287ac5ed
Dan Diaconeasa <mkr@Dans-MacBook-Pro.local>
2023-12-14 12:35:28 +0200

    second commit

--- sellMethod/urls.py
+++ sellMethod/urls.py
@@ -3,10 +3,10 @@ from django.urls import path
 from sellMethod import views
 
 urlpatterns = [
-    path('sell_AI/', views.SellAgentiiImobiliareView.as_view(), name='sell-AI'),
-    path('rent_AI/', views.RentAgentiiImobiliareView.as_view(), name='rent-AI'),
-    path('sell_C0/', views.SellComision0View.as_view(), name='sell-C0'),
-    path('rent_C0/', views.RentComision0View.as_view(), name='rent-C0'),
-    path('sell_AR/', views.SellAnsambluRezidentialView.as_view(), name='sell-AR'),
-    path('rent_AR/', views.RentAnsambluRezidentialView.as_view(), name='rent-AR'),
+    path('agentii_imobiliare/vanzari/', views.SellAgentiiImobiliareView.as_view(), name='sell-AI'),
+    path('agentii_imobiliare/inchirieri/', views.RentAgentiiImobiliareView.as_view(), name='rent-AI'),
+    path('comision_0/vanzari/', views.SellComision0View.as_view(), name='sell-C0'),
+    path('comision_0/inchirieri/', views.RentComision0View.as_view(), name='rent-C0'),
+    path('ansamblu_rezidential/vanzari/', views.SellAnsambluRezidentialView.as_view(), name='sell-AR'),
+    path('ansamblu_rezidential/inchirieri/', views.RentAnsambluRezidentialView.as_view(), name='rent-AR'),
 ]
