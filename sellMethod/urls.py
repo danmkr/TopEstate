5c9654c10461f2ac40200c679c545bd5818d6659
Dan Diaconeasa <mkr@Dans-MacBook-Pro.local>
2023-12-17 14:19:43 +0200

    second commit

--- sellMethod/urls.py
+++ sellMethod/urls.py
@@ -4,7 +4,16 @@ from sellMethod import views
 
 urlpatterns = [
     path('agentii_imobiliare/vanzari/', views.SellAgentiiImobiliareView.as_view(), name='sell-AI'),
+    path('agentii_imobiliare/vanzari/apartamente/', views.SellAgentiiImobiliareApartamenteView.as_view(), name='sell-AI-ap'),
+    path('agentii_imobiliare/vanzari/case_vile/', views.SellAgentiiImobiliareCaseView.as_view(), name='sell-AI-case'),
+    path('agentii_imobiliare/vanzari/terenuri/', views.SellAgentiiImobiliareTerenuriView.as_view(), name='sell-AI-terenuri'),
     path('agentii_imobiliare/inchirieri/', views.RentAgentiiImobiliareView.as_view(), name='rent-AI'),
+    path('agentii_imobiliare/inchirieri/apartamente/', views.RentAgentiiImobiliareApartamenteView.as_view(), name='rent-AI-ap'),
+    path('agentii_imobiliare/inchirieri/case_vile/', views.RentAgentiiImobiliareCaseView.as_view(), name='rent-AI-case'),
+    path('agentii_imobiliare/inchirieri/terenuri/', views.RentAgentiiImobiliareTerenuriView.as_view(), name='rent-AI-terenuri'),
+    path('agentii_imobiliare/apartamente/', views.AgentiiImobiliareApartamenteView.as_view(), name='AI-ap'),
+    path('agentii_imobiliare/case_vile/', views.AgentiiImobiliareCaseView.as_view(), name='AI-case'),
+    path('agentii_imobiliare/terenuri/', views.AgentiiImobiliareTerenuriView.as_view(), name='AI-terenuri'),
     path('comision_0/vanzari/', views.SellComision0View.as_view(), name='sell-C0'),
     path('comision_0/inchirieri/', views.RentComision0View.as_view(), name='rent-C0'),
     path('ansamblu_rezidential/vanzari/', views.SellAnsambluRezidentialView.as_view(), name='sell-AR'),
