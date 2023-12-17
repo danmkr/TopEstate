077c6f598d464da22b92b1a8b099cc2008c03afb
Dan Diaconeasa <mkr@Dans-MacBook-Pro.local>
2023-12-17 14:01:52 +0200

    second commit

--- sellMethod/views.py
+++ sellMethod/views.py
@@ -4,7 +4,30 @@ from django.views.generic import TemplateView, ListView
 from createPost.models import Anunt
 from createPost.views import Comision0View, AgentiiImobiliareView, AnsambluRezidentialView
 
+#region 1. Agentii imobiliare
 
+class AgentiiImobiliareApartamenteView(ListView):
+    template_name = 'sellMethod/AI_ap.html'
+    model = Anunt
+    context_object_name = 'all_announce'
+    def get_queryset(self):
+        return Anunt.objects.filter(tip_anunt='AI', tip_proprietate='apartamente')
+
+class AgentiiImobiliareCaseView(ListView):
+    template_name = 'sellMethod/AI_case.html'
+    model = Anunt
+    context_object_name = 'all_announce'
+    def get_queryset(self):
+        return Anunt.objects.filter(tip_anunt='AI', tip_proprietate='case/vile')
+
+class AgentiiImobiliareTerenuriView(ListView):
+    template_name = 'sellMethod/AI_terenuri.html'
+    model = Anunt
+    context_object_name = 'all_announce'
+    def get_queryset(self):
+        return Anunt.objects.filter(tip_anunt='AI', tip_proprietate='terenuri')
+
+#Vanzari
 class SellAgentiiImobiliareView(ListView):
     template_name = 'sellMethod/sell_AI.html'
     model = Anunt
@@ -12,22 +35,65 @@ class SellAgentiiImobiliareView(ListView):
     def get_queryset(self):
         return Anunt.objects.filter(tip_vanzare='de vanzare', tip_anunt='AI')
 
-
-class SellComision0View(ListView):
-    template_name = 'sellMethod/sell_C0.html'
+class SellAgentiiImobiliareApartamenteView(ListView):
+    template_name = 'sellMethod/sell_AI_ap.html'
     model = Anunt
     context_object_name = 'all_announce'
     def get_queryset(self):
-        return Anunt.objects.filter(tip_vanzare='de vanzare', tip_anunt='C0')
+        return Anunt.objects.filter(tip_vanzare='de vanzare', tip_anunt='AI', tip_proprietate='apartamente')
 
+class SellAgentiiImobiliareCaseView(ListView):
+    template_name = 'sellMethod/sell_AI_case.html'
+    model = Anunt
+    context_object_name = 'all_announce'
+    def get_queryset(self):
+        return Anunt.objects.filter(tip_vanzare='de vanzare', tip_anunt='AI', tip_proprietate='case/vile')
 
+class SellAgentiiImobiliareTerenuriView(ListView):
+    template_name = 'sellMethod/sell_AI_terenuri.html'
+    model = Anunt
+    context_object_name = 'all_announce'
+    def get_queryset(self):
+        return Anunt.objects.filter(tip_vanzare='de vanzare', tip_anunt='AI', tip_proprietate='terenuri')
 
+# Inchirieri
 class RentAgentiiImobiliareView(ListView):
     template_name = 'sellMethod/rent_AI.html'
     model = Anunt
     context_object_name = 'all_announce'
     def get_queryset(self):
         return Anunt.objects.filter(tip_vanzare='de inchiriat', tip_anunt='AI')
+
+class RentAgentiiImobiliareApartamenteView(ListView):
+    template_name = 'sellMethod/rent_AI-ap.html'
+    model = Anunt
+    context_object_name = 'all_announce'
+    def get_queryset(self):
+        return Anunt.objects.filter(tip_vanzare='de inchiriat', tip_anunt='AI', tip_proprietate='apartamente')
+
+class RentAgentiiImobiliareCaseView(ListView):
+    template_name = 'sellMethod/rent_AI_case.html'
+    model = Anunt
+    context_object_name = 'all_announce'
+    def get_queryset(self):
+        return Anunt.objects.filter(tip_vanzare='de inchiriat', tip_anunt='AI', tip_proprietate='case/vile')
+
+class RentAgentiiImobiliareTerenuriView(ListView):
+    template_name = 'sellMethod/rent_AI_terenuri.html'
+    model = Anunt
+    context_object_name = 'all_announce'
+    def get_queryset(self):
+        return Anunt.objects.filter(tip_vanzare='de inchiriat', tip_anunt='AI', tip_proprietate='terenuri')
+
+#endregion
+
+# region 2. Comision 0
+class SellComision0View(ListView):
+    template_name = 'sellMethod/sell_C0.html'
+    model = Anunt
+    context_object_name = 'all_announce'
+    def get_queryset(self):
+        return Anunt.objects.filter(tip_vanzare='de vanzare', tip_anunt='C0')
 
 class RentComision0View(ListView):
     template_name = 'sellMethod/rent_C0.html'
@@ -36,7 +102,10 @@ class RentComision0View(ListView):
     def get_queryset(self):
         return Anunt.objects.filter(tip_vanzare='de inchiriat', tip_anunt='C0')
 
-# ANSAMBLU REZIDENTIAL:
+
+# endregion
+
+#region 3. ANSAMBLU REZIDENTIAL:
 # INCHIRIERI:
 class RentAnsambluRezidentialView(ListView):
     template_name = 'sellMethod/rent_AR.html'
@@ -96,3 +165,5 @@ class AnsambluRezidentialCaseView(ListView):
     context_object_name = 'all_announce'
     def get_queryset(self):
         return Anunt.objects.filter(tip_anunt='AR', tip_proprietate='case/vile')
+
+#endregion
